faf268dab346df4f714db80c30999c54ebe639e4
Kyle Kim <kyle.kim@oicr.on.ca>
2017-09-15 18:26:06 +0000

    Home Banner Particles Fix

--- _app/libs/particles.js
+++ _app/libs/particles.js
@@ -1,50 +1,52 @@
 jQuery(function($){
     $(document).ready(function() {
-        particlesJS.load('particles-js', '/assets/files/particles.json', function() {
-            const height = $('#home-banner-background').height();
-            const img_width = $('#home-banner-background').width();
-            const canvas_el = document.querySelector('#particles-js > .particles-js-canvas-el');
-            if (canvas_el.height > height) {
-                canvas_el.width = img_width;
-                canvas_el.height = height;
-                $('#particles-js > .particles-js-canvas-el').css('width', img_width+'px');
-                $('#particles-js > .particles-js-canvas-el').css('height', height+'px');
-            } else {
-                canvas_el.height = height;
-                $('#particles-js > .particles-js-canvas-el').css('height', height+'px');
-            }
-            const width = $('#home-banner-background').width();
-            $('#particles-js .particles-js-canvas-el').css('position', "absolute");
-            if (width >= 1200) {
-                $('#particles-js .particles-js-canvas-el').css('top', "0px");
-            } else if (width >= 902) {
-                $('#particles-js .particles-js-canvas-el').css('top', "60px");
-            } else {
-                $('#particles-js .particles-js-canvas-el').css('top', "108px");
-            }
-        });
-    });
+        if ($('#particles-js').length) {
+            particlesJS.load('particles-js', '/assets/files/particles.json', function() {
+                const height = $('#home-banner-background').height();
+                const img_width = $('#home-banner-background').width();
+                const canvas_el = document.querySelector('#particles-js > .particles-js-canvas-el');
+                if (canvas_el.height > height) {
+                    canvas_el.width = img_width;
+                    canvas_el.height = height;
+                    $('#particles-js > .particles-js-canvas-el').css('width', img_width+'px');
+                    $('#particles-js > .particles-js-canvas-el').css('height', height+'px');
+                } else {
+                    canvas_el.height = height;
+                    $('#particles-js > .particles-js-canvas-el').css('height', height+'px');
+                }
+                const width = $('#home-banner-background').width();
+                $('#particles-js .particles-js-canvas-el').css('position', "absolute");
+                if (width >= 1200) {
+                    $('#particles-js .particles-js-canvas-el').css('top', "0px");
+                } else if (width >= 902) {
+                    $('#particles-js .particles-js-canvas-el').css('top', "60px");
+                } else {
+                    $('#particles-js .particles-js-canvas-el').css('top', "108px");
+                }
+            });
 
-    $(window).resize(function() {
-        const width = $('#home-banner-background').width();
-        if (width >= 1200) {
-            $('#particles-js .particles-js-canvas-el').css('top', "0px");
-        } else if (width >= 902) {
-            $('#particles-js .particles-js-canvas-el').css('top', "60px");
-        } else {
-            $('#particles-js .particles-js-canvas-el').css('top', "108px");
-        }
-        const height = $('#home-banner-background').height();
-        const img_width = $('#home-banner-background').width();
-        const canvas_el = document.querySelector('#particles-js > .particles-js-canvas-el');
-        if (canvas_el.height > height) {
-            canvas_el.width = img_width;
-                canvas_el.height = height;
-                $('#particles-js > .particles-js-canvas-el').css('width', img_width+'px');
-                $('#particles-js > .particles-js-canvas-el').css('height', height+'px');
-        } else {
-            canvas_el.height = height;
-            $('#particles-js > .particles-js-canvas-el').css('height', height+'px');
-        }
+            $(window).resize(function() {
+                const width = $('#home-banner-background').width();
+                if (width >= 1200) {
+                    $('#particles-js .particles-js-canvas-el').css('top', "0px");
+                } else if (width >= 902) {
+                    $('#particles-js .particles-js-canvas-el').css('top', "60px");
+                } else {
+                    $('#particles-js .particles-js-canvas-el').css('top', "108px");
+                }
+                const height = $('#home-banner-background').height();
+                const img_width = $('#home-banner-background').width();
+                const canvas_el = document.querySelector('#particles-js > .particles-js-canvas-el');
+                if (canvas_el.height > height) {
+                    canvas_el.width = img_width;
+                        canvas_el.height = height;
+                        $('#particles-js > .particles-js-canvas-el').css('width', img_width+'px');
+                        $('#particles-js > .particles-js-canvas-el').css('height', height+'px');
+                } else {
+                    canvas_el.height = height;
+                    $('#particles-js > .particles-js-canvas-el').css('height', height+'px');
+                }
+            });
+        } 
     });
 });
