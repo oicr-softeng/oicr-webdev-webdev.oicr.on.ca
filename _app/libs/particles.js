074e5f0f218b49fa4c5d830ded6118ca8d150040
Kyle Kim <kyle.kim@oicr.on.ca>
2017-09-15 18:07:48 +0000

    Homepage Banner Fix

--- _app/libs/particles.js
+++ _app/libs/particles.js
@@ -2,10 +2,13 @@ jQuery(function($){
     $(document).ready(function() {
         particlesJS.load('particles-js', '/assets/files/particles.json', function() {
             const height = $('#home-banner-background').height();
+            const img_width = $('#home-banner-background').width();
             const canvas_el = document.querySelector('#particles-js > .particles-js-canvas-el');
             if (canvas_el.height > height) {
+                canvas_el.width = img_width;
                 canvas_el.height = height;
-                $('#particles-js > .particles-js-canvas-el').css('height', '100%');
+                $('#particles-js > .particles-js-canvas-el').css('width', img_width+'px');
+                $('#particles-js > .particles-js-canvas-el').css('height', height+'px');
             } else {
                 canvas_el.height = height;
                 $('#particles-js > .particles-js-canvas-el').css('height', height+'px');
@@ -32,10 +35,13 @@ jQuery(function($){
             $('#particles-js .particles-js-canvas-el').css('top', "108px");
         }
         const height = $('#home-banner-background').height();
+        const img_width = $('#home-banner-background').width();
         const canvas_el = document.querySelector('#particles-js > .particles-js-canvas-el');
         if (canvas_el.height > height) {
-            canvas_el.height = height;
-            $('#particles-js > .particles-js-canvas-el').css('height', '100%');
+            canvas_el.width = img_width;
+                canvas_el.height = height;
+                $('#particles-js > .particles-js-canvas-el').css('width', img_width+'px');
+                $('#particles-js > .particles-js-canvas-el').css('height', height+'px');
         } else {
             canvas_el.height = height;
             $('#particles-js > .particles-js-canvas-el').css('height', height+'px');
