83ab40f472078b54aa4514b948d09309cab88e12
Koji Miyauchi <koji.miyauchi@oicr.on.ca>
2017-11-06 14:03:40 -0500

    Update env_theme.js

--- assets/site/js/env_theme.js
+++ assets/site/js/env_theme.js
@@ -1,6 +1,6 @@
 window.UMS_CONFIG = {
     APP_ID: '59fb3476e754513293d2c0ed',
-    API_HOST: 'qa-stratus-www1.github.io:3003',
+    API_HOST: 'qa-stratus-www1.res.oicr.on.ca:3003',
     CONTACT_LINK: 'mailto:webdev.oicr@gmail.com',
 };
 window.extLinkConfig = {
