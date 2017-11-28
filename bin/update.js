3629d6a6ef5681e5b42a244bf7442e2643654da3
William Harvey <william.harvey@oicr.on.ca>
2017-11-28 15:32:54 -0500

    update

--- bin/update.js
+++ bin/update.js
@@ -1,4 +1,21 @@
 #!/usr/bin/env node
 'use strict';
+const chalk = require('chalk');
+const ncp = require('./ncp').ncp;
+const cpy = require('cpy');
+const fs = require('fs');
 
-console.log('coming soon....');
+const options = {
+    clobber: true,
+    filter: (filepath) => {
+        var stats = fs.lstatSync(filepath);
+        return !filepath.includes('app/node_modules') && (stats.isDirectory() || (stats.isFile() && filepath.includes('/core/')))
+    },
+}
+
+ncp('./node_modules/static-cms-baseline/src', './', options, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(chalk.green('Done!'));
+});
--- scripts/update.js
+++ scripts/update.js
@@ -1,2 +1,20 @@
 #!/usr/bin/env node
-console.log('coming soon....')
+const chalk = require('chalk');
+const ncp = require('./ncp').ncp;
+const cpy = require('cpy');
+const fs = require('fs');
+
+const options = {
+    clobber: true,
+    filter: (filepath) => {
+        var stats = fs.lstatSync(filepath);
+        return !filepath.includes('app/node_modules') && (stats.isDirectory() || (stats.isFile() && filepath.includes('/core/')))
+    },
+}
+
+ncp('./node_modules/static-cms-baseline/src', './', options, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(chalk.green('Done!'));
+});
