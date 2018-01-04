6f2626a7084b104f7ef085ec8fd4a2ba7ec7e74a
root <root@oicr.on.ca>
2018-01-04 16:06:36 -0500

    fix small bug and add start-build.sh

--- _api/cron.sh
+++ _api/cron.sh
@@ -5,6 +5,7 @@ PATH=$PATH:/usr/local/bin
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$DIR/sync-mongo.config"
+REPO_PATH=$DIR/../
 
 cd $REPO_PATH
 git pull
---
+++ start-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export JEKYLL_VERSION=3.5
+set -x
+docker run --name=webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_CONTAINER -e VIRTUAL_HOST=webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_HOST -e CERT_NAME=webdevff0b3218aaad11e7abc4cec278b6b50a_ENV_DOMAIN --rm -v $(pwd):/usr/src/app -v /root/.ssh:/root/.ssh -P --expose 4000 -ti jekyll:docker bash -c 'cd /usr/src/app
+sudo apt-get -y install cron
+sudo service cron start
+printf "* * * * * root /usr/src/app/_api/cron.sh > /var/log/sync.log 2>&1\n\n" | sudo tee /etc/cron.d/sync
+sudo bundle update
+sudo jekyll serve -d _site --watch --host=0.0.0.0 --port webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_PORT
+'
