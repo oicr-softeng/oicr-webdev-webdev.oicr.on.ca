558a0f53fc3dd82847bfe67c0f390968a0b2bc55
root <root@oicr.on.ca>
2018-01-05 14:11:09 -0500

    add 2 new webdevff0b3218aaad11e7abc4cec278b6b50a_* variables

--- start-build.sh
+++ start-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export JEKYLL_VERSION=3.5
 set -x
-docker run --name=webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_CONTAINER -e VIRTUAL_HOST=webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_HOST -e CERT_NAME=webdevff0b3218aaad11e7abc4cec278b6b50a_ENV_DOMAIN --rm -v $(pwd):/usr/src/app -v /home/jekyll/.ssh:/home/jekyll/.ssh -P --expose 4000 -ti jekyll:docker bash -c 'cd /usr/src/app
+docker run --name=webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_CONTAINER -e VIRTUAL_HOST=webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_HOST -e CERT_NAME=webdevff0b3218aaad11e7abc4cec278b6b50a_ENV_DOMAIN --rm -v $(pwd):/usr/src/app -v webdevff0b3218aaad11e7abc4cec278b6b50a_GITHUB_KEY_FOLDER:/home/jekyll/.ssh -P --expose 4000 -ti jekyll:docker bash -c 'cd /usr/src/app
 sudo apt-get -y install cron
 sudo apt-get -y install expect
 sudo service cron start
