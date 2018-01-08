709e8cc425a6493f7b9037d7aaaa005ee3ab4f5f
root <root@oicr.on.ca>
2018-01-08 09:38:50 -0500

    move some init commands to Dockfile

--- start-build.sh
+++ start-build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 export JEKYLL_VERSION=3.5
 set -x
-docker run --name=webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_CONTAINER -e VIRTUAL_HOST=webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_HOST -e CERT_NAME=webdevff0b3218aaad11e7abc4cec278b6b50a_ENV_DOMAIN --rm -v $(pwd):/usr/src/app -v webdevff0b3218aaad11e7abc4cec278b6b50a_GITHUB_KEY_FOLDER:/home/jekyll/.ssh -P --expose 4000 -ti jekyll:docker bash -c 'cd /usr/src/app
-sudo apt-get -y install cron
-sudo apt-get -y install expect
-sudo service cron start
-printf "*/10 * * * * jekyll /usr/src/app/_api/cron.sh >> /tmp/sync.log 2>&1\n\n" | sudo tee /etc/cron.d/sync
-sudo bundle update
-sudo jekyll serve -d _site --watch --host=0.0.0.0 --port webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_PORT
+docker run --name=webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_CONTAINER -e VIRTUAL_HOST=webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_HOST -e CERT_NAME=webdevff0b3218aaad11e7abc4cec278b6b50a_ENV_DOMAIN --rm -v $(pwd):/usr/src/app -v webdevff0b3218aaad11e7abc4cec278b6b50a_GITHUB_KEY_FOLDER:/home/jekyll/.ssh -P --expose 4000 -ti jekyll:docker bash -c 'sudo chown -R jekyll.jekyll /usr/src/app
+sudo cron -f
 '
