c1bbbdb87e9b14e0c4fe34db267e363df2443e1f
root <root@oicr.on.ca>
2018-01-10 17:14:51 -0500

    change start-build.sh script

--- start-build.sh
+++ start-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export JEKYLL_VERSION=3.5
 set -x
-docker run --name=webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_CONTAINER -e VIRTUAL_HOST=webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_HOST -e CERT_NAME=webdevff0b3218aaad11e7abc4cec278b6b50a_ENV_DOMAIN --rm -v $(pwd):/usr/src/app -v webdevff0b3218aaad11e7abc4cec278b6b50a_GITHUB_KEY_FOLDER:/home/jekyll/.ssh -P --expose 4000 -ti jekyll:docker bash -c 'sudo chown -R jekyll.jekyll /usr/src/app
+nohup docker run --name=webdevff0b3218aaad11e7abc4cec278b6b50a_BUILD_CONTAINER --rm -v $(pwd):/usr/src/app -v webdevff0b3218aaad11e7abc4cec278b6b50a_GITHUB_KEY_FOLDER:/home/jekyll/.ssh jekyll:docker bash -c 'sudo chown -R jekyll.jekyll /usr/src/app
 sudo cron -f
-'
+' > ../logs/webdevff0b3218aaad11e7abc4cec278b6b50a_BUILD_CONTAINER.log
