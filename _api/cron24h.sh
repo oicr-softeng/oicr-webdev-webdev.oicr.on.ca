e0bd0c454968dedd427a6c328e588f767cfe07f2
root <root@oicr.on.ca>
2018-01-17 15:34:15 -0500

    change start-build.sh add 2 more cron scripts

---
+++ _api/cron24h.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PATH=$PATH:/usr/local/bin
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "$DIR/sync-mongo.config"
+REPO_PATH=$DIR/../
+
+
+if webdevff0b3218aaad11e7abc4cec278b6b50a_CRON_GEN_SITEMAP; then
+  echo "GEN_SITEMAP......"
+fi
+
+if webdevff0b3218aaad11e7abc4cec278b6b50a_CRON_SEND_REPORT; then
+  echo "SEND_REPORT......"
+fi
+
+if webdevff0b3218aaad11e7abc4cec278b6b50a_CRON_EXPIRE_CASE; then
+  echo "EXPIRE_CASE......"
+fi
--- start-build.sh
+++ start-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export JEKYLL_VERSION=3.5
 set -x
-nohup docker run --name=webdevff0b3218aaad11e7abc4cec278b6b50a_BUILD_CONTAINER --rm -v $(pwd):/usr/src/app -v webdevff0b3218aaad11e7abc4cec278b6b50a_GITHUB_KEY_FOLDER:/home/jekyll/.ssh jekyll:docker bash -c 'sudo chown -R jekyll.jekyll /usr/src/app
+nohup docker run --name=webdevff0b3218aaad11e7abc4cec278b6b50a_BUILD_CONTAINER --rm -v $(pwd)/../logs:/var/log -v $(pwd):/usr/src/app -v webdevff0b3218aaad11e7abc4cec278b6b50a_GITHUB_KEY_FOLDER:/home/jekyll/.ssh jekyll:docker bash -c 'sudo chown -R jekyll.jekyll /usr/src/app
+sudo chown -R jekyll.jekyll /var/log
 sudo cron -f
 ' > ../logs/webdevff0b3218aaad11e7abc4cec278b6b50a_BUILD_CONTAINER.log
