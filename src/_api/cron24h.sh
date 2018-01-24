68ee77e92a1da8aebd7bc03cf18fb60d03608c77
Qingda Zou <qingda.zou@oicr.on.ca>
2018-01-24 16:19:58 -0500

    added sitemap generation script and relevant cron changes

--- src/_api/cron24h.sh
+++ src/_api/cron24h.sh
@@ -10,6 +10,13 @@ REPO_PATH=$DIR/../
 
 if webdevff0b3218aaad11e7abc4cec278b6b50a_CRON_GEN_SITEMAP; then
   echo "GEN_SITEMAP......"
+  if [ ! -z "webdevff0b3218aaad11e7abc4cec278b6b50a_PRODUCTION_URL" ]
+  then
+    SITEMAP_OPTIONS=" -s webdevff0b3218aaad11e7abc4cec278b6b50a_PRODUCTION_URL -o ../sitemap.xml"
+  else
+    SITEMAP_OPTIONS=" -s https://webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_HOST -l -o ../sitemap.xml"
+  fi
+  node sitemapCron.js $SITEMAP_OPTIONS
 fi
 
 if webdevff0b3218aaad11e7abc4cec278b6b50a_CRON_SEND_REPORT; then
