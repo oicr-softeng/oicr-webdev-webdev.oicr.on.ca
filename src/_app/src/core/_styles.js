bb9d1508033b23cfe58259b7233f6bb4fa83617b
Koji Miyauchi <kmiyauchi@odm-kmiyauchi.ad.oicr.on.ca>
2018-05-17 15:21:33 -0400

    updating

--- src/_app/src/core/_styles.js
+++ src/_app/src/core/_styles.js
@@ -1,7 +1,2 @@
 // Load required stylesheets
-// import './styles.scss';
-
-import 'oicr-ui-core/dist/_oicr-theme-variables.scss';
-import '../../../_sass/core/bootstrap-variables.scss';
-import 'wfui-react/dist/_wfui-react.scss';
-import 'oicr-ui-core/dist/_oicr-ui-core.scss';
+import './styles.scss';
