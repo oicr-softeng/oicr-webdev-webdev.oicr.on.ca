4cc0ddfacf36b0ac5060fc9e7bdb3bcdf77595ba
Joseph Yamada <joseph.yamada@oicr.on.ca>
2018-06-13 11:47:48 -0400

    Update env_theme.js

--- assets/site/js/env_theme.js
+++ assets/site/js/env_theme.js
@@ -1,7 +1,7 @@
 window.UMS_CONFIG = {
     APP_ID: '59fb3476e754513293d2c0ed',
 //    APP_ID: 'webdevff0b3218aaad11e7abc4cec278b6b50a_APP_ID',
-    API_HOST: 'api.labs.oicr.on.ca:443',
+    API_HOST: 'api.nextlabs.oicr.on.ca:443',
 //    API_HOST: 'webdevff0b3218aaad11e7abc4cec278b6b50a_API_HOST:webdevff0b3218aaad11e7abc4cec278b6b50a_API_DOCKER_PORT',
     CONTACT_LINK: 'webdev@oicr.on.ca',
 //    CONTACT_LINK: 'webdevff0b3218aaad11e7abc4cec278b6b50a_MAIN_CONTACT',
