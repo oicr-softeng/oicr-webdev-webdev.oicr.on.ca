869a1bc01de40f43467cef3c2abf4c42aa8cf07d
qingda-oicr <34283849+qingda-oicr@users.noreply.github.com>
2018-07-17 11:55:05 -0400

    Update entries.js

--- src/_app/configs/entries.js
+++ src/_app/configs/entries.js
@@ -2,7 +2,7 @@ module.exports = {
     base_styles: ['./src/core/_styles'],
     base: ['./src/core/_base'],
     //forms: ['./src/core/_forms'],
-    //ums: ['./src/site/_ums'],
+    //ums: ['./src/core/_ums'],
     //bundle: ['./src/site/_all', './src/site/_share', './src/site/_news'],
-    //search: ['./src/site/_search'],
+    //search: ['./src/core/_search'],
 };
