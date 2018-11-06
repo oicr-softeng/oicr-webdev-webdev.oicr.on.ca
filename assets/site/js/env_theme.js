ff9d66eeabc3f9283ecc7eff138c51e2eb1b5e28
Joseph Yamada <joe.yamada@gmail.com>
2018-11-06 15:17:19 -0500

    Update env_theme.js

--- assets/site/js/env_theme.js
+++ assets/site/js/env_theme.js
@@ -1,10 +1,9 @@
 window.APP_CONFIG = {
     APP_ID: '59fb3476e754513293d2c0ed',
-    API_HOST:
-        'webdevcr-api.koji.vm',
-    CONTACT_LINK: 'webdevff0b3218aaad11e7abc4cec278b6b50a_MAIN_CONTACT',
-    CMUI_ENABLED: true,
-    FORM_ENABLED: true
+    API_HOST: 'api.nextlabs.oicr.on.ca:443',
+    CONTACT_LINK: 'webdev@oicr.on.ca',
+    CMUI_ENABLED: YES,
+    FORM_ENABLED: YES
 };
 window.extLinkConfig = {
     extAlert: '_Alert',
