1e54bdc42b63956c6f424745ef3ced9cc79c2b3f
Koji Miyauchi <kmiyauchi@odm-kmiyauchi.ad.oicr.on.ca>
2018-11-06 10:54:24 -0500

    Re-bundle app

--- assets/site/js/env_theme.js
+++ assets/site/js/env_theme.js
@@ -1,10 +1,10 @@
 window.APP_CONFIG = {
-    APP_ID: 'webdevff0b3218aaad11e7abc4cec278b6b50a_APP_ID',
+    APP_ID: '59fb3476e754513293d2c0ed',
     API_HOST:
-        'webdevff0b3218aaad11e7abc4cec278b6b50a_API_HOST:webdevff0b3218aaad11e7abc4cec278b6b50a_API_DOCKER_PORT',
+        'webdevcr-api.koji.vm',
     CONTACT_LINK: 'webdevff0b3218aaad11e7abc4cec278b6b50a_MAIN_CONTACT',
-    CMUI_ENABLED: webdevff0b3218aaad11e7abc4cec278b6b50a_CMUI_ENABLED,
-    FORM_ENABLED: webdevff0b3218aaad11e7abc4cec278b6b50a_FORM_ENABLED
+    CMUI_ENABLED: true,
+    FORM_ENABLED: true
 };
 window.extLinkConfig = {
     extAlert: '_Alert',
