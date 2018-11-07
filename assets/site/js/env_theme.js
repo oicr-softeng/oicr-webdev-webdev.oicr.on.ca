3b202099f19ab21feecd21a9d98f93b44e5c9679
Kaman Wu <kaman.wu@oicr.on.ca>
2018-11-07 15:11:45 -0500

    Update env_theme.js

--- assets/site/js/env_theme.js
+++ assets/site/js/env_theme.js
@@ -1,6 +1,7 @@
 window.APP_CONFIG = {
     APP_ID: '59fb3476e754513293d2c0ed',
     API_HOST: 'api.nextlabs.oicr.on.ca:443',
+    GITHUB_LOGIN_URL: 'https://api.nextlabs.oicr.on.ca:443/user/auth/github?appid=59fb3476e754513293d2c0ed&continue=https%3A%2F%2Fwebdev.oicr.on.ca%2Fuser%2F%23%2Fdashboard',
     CONTACT_LINK: 'webdev@oicr.on.ca',
     CMUI_ENABLED: true,
     FORM_ENABLED: true
