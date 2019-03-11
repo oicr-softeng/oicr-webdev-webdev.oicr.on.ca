2a812d3a7e6013f81a69e292cb664207e37ef940
Koji Miyauchi <kmiyauchi@odm-kmiyauchi.ad.oicr.on.ca>
2019-03-11 15:42:16 -0400

    Fixed webpack optimization issues.

---
+++ _app/src/core/webpack.styles.config.js
@@ -0,0 +1,65 @@
+require('babel-polyfill');
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const transpiling = require('../../configs/transpiling');
+const os = require('os');
+
+const moduleExclude = new RegExp(
+    'node_modules/(?!(' + transpiling.join('|') + ')/).*'
+);
+
+module.exports = env => {
+
+    /**
+     * Plugin setting
+     */
+    var plugins = [
+        new ExtractTextPlugin('../css/core.bundle.css'),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+        new webpack.DefinePlugin(
+            {
+                'process.env': {
+                    NODE_ENV: JSON.stringify('production')
+                }
+            }
+        ),
+        new UglifyJsPlugin({
+            uglifyOptions: {
+                warnings: false,
+                ie8: false
+            },
+            parallel: os.cpus().length,
+            cache: true,
+        }),
+    ];
+
+    return {
+        entry: ['./src/core/_styles'],
+        output: {
+            filename: '.base_styles',
+            path: path.resolve('../assets/dist/js/'),
+            publicPath: '/assets/dist/js/'
+        },
+        resolve: {
+            extensions: ['.js']
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.scss$|\.css$/,
+                    loader: ExtractTextPlugin.extract('css-loader!sass-loader')
+                }
+            ]
+        },
+        plugins: plugins,
+        node: {
+            fs: 'empty',
+            child_process: 'empty'
+        }
+    };
+};
