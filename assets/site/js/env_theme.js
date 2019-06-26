e4d38c985931a73c143a244093bae100e61da79b
Joseph Yamada <joseph.yamada@oicr.on.ca>
2019-06-26 14:21:37 -0400

    Update env_theme.js

--- assets/site/js/env_theme.js
+++ assets/site/js/env_theme.js
@@ -1,11 +1,12 @@
 window.APP_CONFIG = {
-    APP_ID: '59fb3476e754513293d2c0ed',
-    API_HOST: 'api.nextlabs.oicr.on.ca:443',
-    GITHUB_LOGIN_URL: 'https://api.nextlabs.oicr.on.ca:443/user/auth/github?appid=59fb3476e754513293d2c0ed&continue=https%3A%2F%2Fwebdev.oicr.on.ca%2Fuser%2F%23%2Fdashboard',
-    CONTACT_LINK: 'webdev@oicr.on.ca',
-    CMUI_ENABLED: true,
-    FORM_ENABLED: true,
-    FROALA_ACTIVATION_KEY: 'bJ3B10B7D7cD3F3C2C1G3C5C3B6E2E2vdenC-22A8E-13E-13B1pu==',
+    APP_ID: 'webdevff0b3218aaad11e7abc4cec278b6b50a_APP_ID',
+    API_HOST:
+        'webdevff0b3218aaad11e7abc4cec278b6b50a_API_HOST:webdevff0b3218aaad11e7abc4cec278b6b50a_API_DOCKER_PORT',
+    GITHUB_LOGIN_URL: '//webdevff0b3218aaad11e7abc4cec278b6b50a_API_HOST:webdevff0b3218aaad11e7abc4cec278b6b50a_API_DOCKER_PORT/user/auth/github?appid=webdevff0b3218aaad11e7abc4cec278b6b50a_APP_ID&continue=https%3A%2F%2Fwebdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_HOST%2Fuser%2F%23%2Fdashboard',
+    CONTACT_LINK: 'webdevff0b3218aaad11e7abc4cec278b6b50a_MAIN_CONTACT',
+    CMUI_ENABLED: webdevff0b3218aaad11e7abc4cec278b6b50a_CMUI_ENABLED,
+    FORM_ENABLED: webdevff0b3218aaad11e7abc4cec278b6b50a_FORM_ENABLED,
+    FROALA_ACTIVATION_KEY: 'webdevff0b3218aaad11e7abc4cec278b6b50a_FROALA_ACTIVATION_KEY'
 };
 window.extLinkConfig = {
     extAlert: '_Alert',
